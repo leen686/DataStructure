8163576ce7cf19b0a0149785334cbd4d304be07c
leen686 <leenmoh305@gmail.com>
2025-11-04 19:58:40 +0300

    wow

---
+++ DataStructureProject/src/datastructureproject/DataStructureProject.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package datastructureproject;
+
+/**
+ *
+ * @author leenm
+ */
+public class DataStructureProject {
+
+   
+    public static void main(String[] args) {
+        System.out.println("الله يستر");
+    }
+    
+}
